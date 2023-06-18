844e4b27fd23694a139cc44f683cfbe1275afe03
Aleksandr Eismont <alexander_eismont@mail.ru>
2023-06-18 19:46:40 +0200

    Removed unused UNet version

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,6 @@
 from .model import Model
 from .dense_model import DenseModel, DenseModelDropout
-from .unet import UNet, UNet2
+from .unet import UNet
 from .multi_path_unet import MultiPathUNet
-
 from .encoder_decoder import EncoderDecoder
 from .multi_path_encoder_decoder import MultiPathEncoderDecoder
