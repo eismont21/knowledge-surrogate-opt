8594efc2a4455c7d81ba18e7567f52f71230505d
Aleksandr Eismont <alexander_eismont@mail.ru>
2023-06-27 17:13:37 +0200

    Removed resizing layer

--- src/models/unet.py
+++ src/models/unet.py
@@ -7,14 +7,12 @@ from src.layers.concrete_dropout import ConcreteSpatialDropout2D, get_weight_reg
 class UNet(Model):
     def __init__(self, name: str, input_dim: int, output_dim, x_train, base_filters: int = 64,
                  activation: str = 'relu', initializer: str = 'he_normal',
-                 encoding: str = 'naive', positional_encoding: int = 0, is_mc_dropout: bool = False,
-                 upsampling_interpolation: str = 'nearest'):
+                 encoding: str = 'naive', positional_encoding: int = 0, is_mc_dropout: bool = False):
         self.base_filters = base_filters
         self.initializer = initializer
         self.activation = activation
         self.encoding = encoding
         self.positional_encoding = positional_encoding
-        self.upsampling_interpolation = upsampling_interpolation
         self.x_train = x_train
         self.wr = get_weight_regularizer(self.x_train.shape[0], l=1e-2, tau=1.0)
         self.dr = get_dropout_regularizer(self.x_train.shape[0], tau=1.0)
@@ -47,7 +45,7 @@ class UNet(Model):
         c1 = tf.keras.layers.Conv2D(filters=self.base_filters, kernel_size=(3, 3), activation=self.activation,
                                     kernel_initializer=self.initializer, padding='same')(c1)
         c1 = tf.keras.layers.BatchNormalization()(c1)
-        p1 = tf.keras.layers.MaxPooling2D((2, 2))(c1)
+        p1 = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(c1)
 
         c2_layer = tf.keras.layers.Conv2D(filters=self.base_filters * 2, kernel_size=(3, 3), activation=self.activation,
                                           kernel_initializer=self.initializer, padding='same')
@@ -57,7 +55,7 @@ class UNet(Model):
         c2 = tf.keras.layers.Conv2D(filters=self.base_filters * 2, kernel_size=(3, 3), activation=self.activation,
                                     kernel_initializer=self.initializer, padding='same')(c2)
         c2 = tf.keras.layers.BatchNormalization()(c2)
-        p2 = tf.keras.layers.MaxPooling2D((2, 2))(c2)
+        p2 = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(c2)
 
         c3_layer = tf.keras.layers.Conv2D(filters=self.base_filters * 4, kernel_size=(3, 3), activation=self.activation,
                                           kernel_initializer=self.initializer, padding='same')
@@ -77,7 +75,7 @@ class UNet(Model):
         c4 = tf.keras.layers.Conv2D(filters=self.base_filters * 8, kernel_size=(3, 3), activation=self.activation,
                                     kernel_initializer=self.initializer, padding='same')(c4)
         c4 = tf.keras.layers.BatchNormalization()(c4)
-        p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
+        p4 = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(c4)
 
         c5_layer = tf.keras.layers.Conv2D(filters=self.base_filters * 16, kernel_size=(3, 3),
                                           activation=self.activation,
@@ -105,9 +103,10 @@ class UNet(Model):
                                                    padding='same')
         u7 = ConcreteSpatialDropout2D(u7_layer, weight_regularizer=self.wr, dropout_regularizer=self.dr,
                                       is_mc_dropout=self.is_mc_dropout)(c6)
-        c3_resized = tf.keras.layers.Resizing(u7.shape[1], u7.shape[2], interpolation=self.upsampling_interpolation,
-                                              crop_to_aspect_ratio=False)(c3)
-        u7 = tf.keras.layers.concatenate([u7, c3_resized], axis=3)
+        u7 = tf.keras.layers.Conv2D(filters=self.base_filters * 4, kernel_size=(2, 2), activation=self.activation,
+                                    kernel_initializer=self.initializer, padding='valid')(u7)
+        u7 = tf.keras.layers.BatchNormalization()(u7)
+        u7 = tf.keras.layers.concatenate([u7, c3], axis=3)
         c7 = tf.keras.layers.Conv2D(filters=self.base_filters * 4, kernel_size=(3, 3), activation=self.activation,
                                     kernel_initializer=self.initializer, padding='same')(u7)
         c7 = tf.keras.layers.BatchNormalization()(c7)
@@ -119,14 +118,12 @@ class UNet(Model):
                                                    padding='same')
         u8 = ConcreteSpatialDropout2D(u8_layer, weight_regularizer=self.wr, dropout_regularizer=self.dr,
                                       is_mc_dropout=self.is_mc_dropout)(c7)
-        c2_resized = tf.keras.layers.Resizing(u8.shape[1], u8.shape[2], interpolation=self.upsampling_interpolation,
-                                              crop_to_aspect_ratio=False)(c2)
-        u8 = tf.keras.layers.concatenate([u8, c2_resized], axis=3)
+        u8 = tf.keras.layers.concatenate([u8, c2], axis=3)
         c8 = tf.keras.layers.Conv2D(filters=self.base_filters * 2, kernel_size=(3, 3), activation=self.activation,
                                     kernel_initializer=self.initializer, padding='same')(u8)
         c8 = tf.keras.layers.BatchNormalization()(c8)
         c8 = tf.keras.layers.Conv2D(filters=self.base_filters * 2, kernel_size=(3, 3), activation=self.activation,
-                                    kernel_initializer=self.initializer, padding='valid')(c8)
+                                    kernel_initializer=self.initializer, padding='same')(c8)
         c8 = tf.keras.layers.BatchNormalization()(c8)
 
         u9_layer = tf.keras.layers.Conv2DTranspose(filters=self.base_filters, kernel_size=(2, 2), strides=(2, 2),
