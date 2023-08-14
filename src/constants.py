b98704d9004b83d210588f803e6a933832095c24
Aleksandr Eismont <alexander_eismont@mail.ru>
2023-08-14 13:04:30 +0200

    Updated constants

--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,6 @@ ENCODING_PATH = os.path.join(ROOT_DIR, 'data', 'encoding')
 LENGTHS_PATH = os.path.join(ROOT_DIR, 'Austausch_IPD_Optimierung', 'u_spring', 'U_spring.csv')
 
 MATRIX_SHAPE = (460, 300)
+
+INPUT_SHAPE = (60, )
+OUTPUT_SHAPE = (460, 300, 1)
