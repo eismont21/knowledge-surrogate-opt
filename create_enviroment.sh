3cdf4debd7541974b6d5348c8275d2aee2927de6
Aleksandr Eismont <alexander_eismont@mail.ru>
2023-10-17 17:36:11 +0200

    Added package versions in env

--- create_enviroment.sh
+++ create_enviroment.sh
@@ -10,7 +10,7 @@ mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo 'CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 echo 'export LD_LIBRARY_PATH=$CONDA_PREFIX/lib/:$CUDNN_PATH/lib:$LD_LIBRARY_PATH' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 source $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-pip install tensorflow==2.13.*
+pip install tensorflow==2.13.0
 conda install -c nvidia cuda-nvcc=11.3.58
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 printf 'export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/lib/\n' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
